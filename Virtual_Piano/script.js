028fa94a3c45826fa25c090957371c8388e743eb
1112luke <lukescholler@gmail.com>
2023-10-24 21:36:57 -0400

    added keyboard with slider

--- Virtual_Piano/script.js
+++ Virtual_Piano/script.js
@@ -1,37 +1,78 @@
 
 keys = [];
-keyboardheight = 100;
 
-function createKeys(){
+keyboardHeight = window.innerHeight * 0.4;
+
+numkeys = 20;
+
+//initialize slider count
+document.getElementById("count").innerHTML = `<h2>${numkeys}</h2>`;
+
+function drawKeys(){
+
+    //remove all previous keys from screen
+    keys = [];
+    document.getElementById("pianoBox").innerHTML = "";
 
     //create white keys
-
-    numkeys = 10
-    keyWidth = window.innerWidth/numkeys;
-    keyHeight = 350;
+    whiteKeyWidth = window.innerWidth/numkeys;
+    whiteKeyHeight = keyboardHeight;
+    blackKeyWidth = whiteKeyWidth*2/3;
+    blackKeyHeight = 0.8*whiteKeyHeight;
 
     for(var i = 0; i < numkeys; i++){
         var newel = document.createElement("div");
-        newel.style.width = `${keyWidth}px`;
-        newel.style.height = `${keyHeight}px`;
+        newel.style.width = `${whiteKeyWidth}px`;
+        newel.style.height = `${whiteKeyHeight}px`;
         newel.style.background = "white";
         newel.style.color = "white";
         newel.style.position = "absolute";
         newel.style.border = "2px solid black"
-        newel.style.left = `${i*keyWidth}px`;
+        newel.style.left = `${i*whiteKeyWidth}px`;
         newel.style.bottom = "0px"
         newel.innerHTML = "";
         newel.setAttribute("id", "white");
-        document.getElementById("wrapper").appendChild(newel);
+        document.getElementById("pianoBox").appendChild(newel);
         keys.push(newel);
     }
 
     //create black keys
+
+    var whitelength = keys.length
+
+    for(var i = 1; i < whitelength; i++){
+        //check if they white key is a first, second, third, fifth, or sixth key
+        if(i % 7 == 1 || i % 7 == 2 || i % 7 == 3 || i % 7 == 5 || i % 7 == 6){
+            //create black key upper right of the corresponding white key
+            var newel = document.createElement("div");
+            newel.style.width = `${blackKeyWidth}px`;
+            newel.style.height = `${blackKeyHeight}px`;
+            newel.style.background = "black";
+            newel.style.color = "black";
+            newel.style.position = "absolute";
+            newel.style.border = "2px solid black"
+            newel.style.left = `${i*whiteKeyWidth-blackKeyWidth/2}px`;
+            newel.style.bottom = `${whiteKeyHeight-blackKeyHeight}px`
+            newel.innerHTML = "";
+            newel.setAttribute("id", "black");
+            document.getElementById("pianoBox").appendChild(newel);
+            keys.push(newel);
+        }
+    }
 }
+
+//handle slider
+var slider = document.getElementById("myRange");
+
+slider.addEventListener("input", function(){
+    numkeys = this.value;
+    document.getElementById("count").innerHTML = `<h2>${numkeys}</h2>`;
+    drawKeys();
+})
 
 console.log(keys);
 
-createKeys();
+drawKeys();
 
 function animate(){
 }

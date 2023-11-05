5d613c43795f287ed1f273d6d5d624c7b046c46f
Zomb42 <dericks1real@gmail.com>
2023-11-05 14:57:41 -0500

    Made boring red square in LS become gradient

--- CanvasPractice/script.js
+++ CanvasPractice/script.js
@@ -12,14 +12,15 @@ function move(){
 
     ctx.beginPath();
     ctx.fillStyle = "rgba(0, 0, 0, 255)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);    
     ctx.stroke();
-    var grd = ctx.createRadialGradient(75,50,5,90,60,100);
+    rectwidth = 150
+    rectheight =100
+    var grd = ctx.createRadialGradient(75+Math.abs(posx),50+Math.abs(posy),5,90+Math.abs(posx),60+Math.abs(posy),100);
     grd.addColorStop(0,"red");
     grd.addColorStop(1,"white");
 
-    rectwidth = 150
-    rectheight =100
+    ctx.fillRect(0, 0, canvas.width, canvas.height);    
+
     ctx.rect(posx, posy, rectwidth, rectheight);
     ctx.fillStyle = grd;
     ctx.fill();

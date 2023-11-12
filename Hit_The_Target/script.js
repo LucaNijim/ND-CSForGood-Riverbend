f51ef0783c1e13813e62e064e62fddd2034838e0
cht-au <145236495+cht-au@users.noreply.github.com>
2023-11-12 14:57:06 -0500

    Improvements and fix bugs
    Fix bugs with variables, rename button, and added comments

--- Hit_The_Target/script.js
+++ Hit_The_Target/script.js
@@ -5,33 +5,38 @@ var maxRangeDisplay = document.getElementById('max');
 let belowRangeOutput = document.getElementById('belowRange');
 let inRangeOutput = document.getElementById('inRange');
 let aboveRangeOutput = document.getElementById('aboveRange');
-let startingNumber, baseTarget;
-let min;
-let max;
+let startingNumber, baseTarget, min, max;
 let belowRange = 0;
 let inRange = 0;
 let aboveRange = 0;
-let inRangeGuesses =[];
+let inRangeGuesses = [];
 
 
 
 function onButtonClick() {
+  //randomize the starting number and the range 
   startingNumber = 1 + Math.floor(Math.random() * 12);
   baseTarget = 1 + Math.floor(Math.random() * 12);
   min = (startingNumber * baseTarget) + 1;
   max = (startingNumber * (baseTarget+4)) - 1;
-
   startingNumberDisplay.textContent = startingNumber;
   minRangeDisplay.textContent = min;
   maxRangeDisplay.textContent = max;
-  inRangeGuesses =[]
-  belowRange,inRange,aboveRange = 0;
+
+  //reset everything when the button is clicked
+  inRangeGuesses =[];
+  belowRange = 0;
+  inRange = 0;
+  aboveRange = 0;
   belowRangeOutput.textContent = 0;
   inRangeOutput.textContent = 0;
   aboveRangeOutput.textContent = 0;
+  document.getElementById('userInput').value = '';
 }
 
 function multiplyCheck() {
+
+  //grab user's input and check if it's in range or one number out of range
   let input = document.getElementById('userInput').valueAsNumber;
   if((input*startingNumber == min-1) && (belowRange<1)) {
     belowRangeOutput.textContent =belowRange + 1;
@@ -45,8 +50,8 @@ function multiplyCheck() {
     inRangeOutput.textContent = inRange;
   }
 }
-  
-  
+
+//buttons
 const buttonRandomize = document.getElementById('randomize')
 buttonRandomize.addEventListener('click', onButtonClick);
 
